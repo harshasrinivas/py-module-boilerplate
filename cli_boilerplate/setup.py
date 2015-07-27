f2e7284629bb489d3f340c27c283f12623743fa2
95harsha95 <95harsha95@gmail.com>
2015-07-27 15:55:36 +0530

    fixed parsing bug

--- cli_boilerplate/setup.py
+++ cli_boilerplate/setup.py
@@ -1,26 +1,24 @@
-"""installation setup file"""
+  from setuptools import setup
 
-from setuptools import setup
-
-setup(name='<app>',
-      description='<description>',
-      version='0.1.0',
-      author='Harsha Srinivas',
-      author_email='95harsha95@gmail.com',
-      packages=['<app>'],
-      entry_points={
-          'console_scripts': ['<app>=<app>:main'],
-      },
-      url='https://github.com/harshasrinivas/<app>/',
-      keywords=[ 'CLI', 'python'],
-      classifiers=[
-          'Operating System :: POSIX',
-          'Environment :: Console',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Topic :: Utilities',
-          'Topic :: Software Development :: Libraries :: Python Modules',
+  setup(name='<app>',
+        description='<description>',
+        version='0.1.0',
+        author='Harsha Srinivas',
+        author_email='95harsha95@gmail.com',
+        packages=['<app>'],
+        entry_points={
+            'console_scripts': ['<app>=<app>:main'],
+        },
+        url='https://github.com/harshasrinivas/<app>/',
+        keywords=[ 'CLI', 'python'],
+        classifiers=[
+            'Operating System :: POSIX',
+            'Environment :: Console',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Topic :: Utilities',
+            'Topic :: Software Development :: Libraries :: Python Modules',
       ],)
--- script.sh
+++ script.sh
@@ -1,9 +1,29 @@
 #! /bin/sh
 
-sed -i 's/<app>/'$1'/g' cli_boilerplate/setup.py
-sed -i 's/<app>/'$1'/g' cli_boilerplate/README.md
-sed -i 's/<app>/'$1'/g' cli_boilerplate/README.rst
-cat cli_boilerplate/setup.py | sed -e "s/author='Harsha Srinivas',/author='"$2"',/" >> cli_boilerplate/setup.py
-cat cli_boilerplate/setup.py | sed -e "s/author_email='95harsha95@gmail.com',/author_email='"$3"',/" >> cli_boilerplate/setup.py
-sed -i '/url/c\\turl="'$4'"' cli_boilerplate/setup.py
+if [ -z "$1" ]
+then
+	echo "Package Name required"
+else
+	sed -i 's/<app>/'$1'/g' cli_boilerplate/setup.py
+	sed -i 's/<app>/'$1'/g' cli_boilerplate/README.md
+	sed -i 's/<app>/'$1'/g' cli_boilerplate/README.rst
+	if [ -z "$2" ]
+	then
+		:
+	else
+		sed -i '/author=/c\\tauthor="'$2'",' cli_boilerplate/setup.py
+		if [ -z "$3" ]
+		then
+			:
+		else
+			sed -i '/author_email=/c\\tauthor_email="'$3'",' cli_boilerplate/setup.py
+			if [ -z "$4" ]
+			then
+				:
+			else
+				sed -i '/url/c\\turl="'$4'",' cli_boilerplate/setup.py
+			fi
+		fi
+	fi
+fi
 mv cli_boilerplate/app/ cli_boilerplate/$1
